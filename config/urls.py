7e753a2bccb833ab947eae594d49ac74446389cf
Eng. Njokosi Jones Kawunju <eng.njokosi@gmail.com>
2021-08-13 09:22:13 +0300

    :book: Change api documentation route

--- config/urls.py
+++ config/urls.py
@@ -53,7 +53,7 @@ urlpatterns += [
     path("api-auth/", include("rest_framework.urls")),
     path("api/v1/auth/", include("dj_rest_auth.urls")),
     path("api/social/login/", include("users.urls")),  # user authentication urls
-    path("api/v1/documentation/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path("api/documentation/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
     # path("openapi", get_schema_view(
     #     title="Users API",
     #     description="A sample API for accessing users",
