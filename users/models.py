0539bfa1e9b3ef411e80c4aabbbf79eeb04b17d2
Eng. Njokosi Jones Kawunju <eng.njokosi@gmail.com>
2021-08-13 07:01:32 +0300

    :zap: Update custom user model fields

--- users/models.py
+++ users/models.py
@@ -12,7 +12,18 @@ class CustomUser(AbstractUser):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
+    active = models.BooleanField(_('active'), default=True)
+
+    is_staff = models.BooleanField(_('is_staff'), default=False)
+    is_superuser = models.BooleanField(_('is_superuser'), default=False)
+
+    created_on = models.DateTimeField(_('created on'), auto_now_add=True, blank=True, null=True)
+    updated_at = models.DateTimeField(_('updated on'), auto_now=True)
+
     objects = CustomUserManager()
 
     def __str__(self):
         return self.email
+
+    class Meta:
+        verbose_name = "User"
