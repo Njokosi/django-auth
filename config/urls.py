2014959effab927b796e9f78351d2a74a9ff0897
Eng. Njokosi Jones Kawunju <eng.njokosi@gmail.com>
2021-08-13 08:16:52 +0300

    :tada: Support social auth

--- config/urls.py
+++ config/urls.py
@@ -52,6 +52,7 @@ urlpatterns = [
 urlpatterns += [
     path("api-auth/", include("rest_framework.urls")),
     path("api/v1/auth/", include("dj_rest_auth.urls")),
+    path("api/social/login/", include("users.urls")),  # user authentication urls
     path("api/v1/documentation/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
     # path("openapi", get_schema_view(
     #     title="Users API",
--- users/serializers.py
+++ users/serializers.py
@@ -1,2 +1,21 @@
+from django.conf import settings
 from .models import CustomUser
-from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
+
+
+class CustomUserModelSerializer(ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = [
+            "id",
+            "email",
+            "password",
+        ]
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(
+            validated_data["email"],
+            validated_data["password"]
+        )
+
+        return user
